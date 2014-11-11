a32100fc7d6b1063eea8bba8c86133b5fb70219e
Alexey Sveshnikov <alexey.sveshnikov@gmail.com>
2014-11-12 01:35:56 +0300

    don't do lower() on url in normalize()

--- smarturlfield/__init__.py
+++ smarturlfield/__init__.py
@@ -165,7 +165,7 @@ class MultipleSmartURLDbField(models.TextField):
 
 
 def normalize_url(s):
-    s = s.strip().lower()
+    s = s.strip()
     if '://' in s or not s:
         return s
     else:
