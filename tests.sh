908140a424c4d229831491168c19e2d369738736
Alexey Sveshnikov <alexey.sveshnikov@gmail.com>
2017-04-12 16:43:02 +0300

    Remove script tests.sh (use tox instead)

--- tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-DJANGO_SETTINGS_MODULE=example.settings django-admin.py test --pythonpath=./ --failfast
-DJANGO_SETTINGS_MODULE=example.settings python smarturlfield/__init__.py
