8cef96067cbf7c5b8ddefd76783fe822f62e24ff
Alexey Sveshnikov <alexey.sveshnikov@gmail.com>
2017-04-14 20:07:08 +0300

    Added bumpversion config file. Reset version number to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-smarturlfield',
-    version='1.1',
+    version='0.1.0',
     author='Maxim Oransky',
     author_email='maxim.oransky@gmail.com',
     url='https://github.com/shantilabs/django-smarturlfield',
