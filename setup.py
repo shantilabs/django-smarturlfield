9303f38035e67b67c4bbfd871b951ea32ce687f6
Alexey Sveshnikov <alexey.sveshnikov@gmail.com>
2017-04-17 12:16:29 +0300

    Add classifiers (python and django versions)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-smarturlfield',
-    version='1.0.0',
+    version='1.0.1',
     author='Maxim Oransky',
     author_email='maxim.oransky@gmail.com',
     description="Django model and form fields that performs URL normalization. For example it adds 'http://' prefix and forces punicode encoding.",
@@ -14,5 +14,14 @@ setup(
     ],
     keywords=[
         'django', 'django-fields', 'url'
+    ],
+    classifiers=[
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
